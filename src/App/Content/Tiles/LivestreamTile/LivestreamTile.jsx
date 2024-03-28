d522d555f58346d1b0393b0ee54ad787a4b35b46
Clément Ababou <clement.ababou@outlook.com>
2024-03-28 19:13:37 +0100

    Added favorite icon on the livestream tile

--- src/App/Content/Tiles/LivestreamTile/LivestreamTile.jsx
+++ src/App/Content/Tiles/LivestreamTile/LivestreamTile.jsx
@@ -4,6 +4,9 @@ import Button from "../../../Button/Button";
 import {getStreamUptime} from "../../../Util";
 import {useMouseOver} from "../../../hooks/useMouseOver";
 import classNames from "classnames";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar as emptyStar} from "@fortawesome/free-regular-svg-icons";
+import {faStar as solidStar} from "@fortawesome/free-solid-svg-icons";
 
 export default React.memo(function LivestreamTile({livestream}) {
 	const ref = useRef();
@@ -15,8 +18,20 @@ export default React.memo(function LivestreamTile({livestream}) {
 				url: `https://www.twitch.tv/${livestream.user_login}`
 			})
 		}}>
-			<img className={classNames(styles.streamPic, isHovered ? styles.hovered : false)} src={livestream.thumbnail_url.replace('{width}', '128').replace('{height}', '72')}/>
-			<div className={styles.streamerName} title={(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}>{(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}</div>
+			<div className={styles.streamPicContainer} style={{ display: 'inline-block' }}>
+				<img className={classNames(styles.streamPic, isHovered ? styles.hovered : false)} src={livestream.thumbnail_url.replace('{width}', '128').replace('{height}', '72')}/>
+				{
+					isHovered ? (
+						<FontAwesomeIcon className={classNames(styles.favoriteIcon, livestream.isFavorite ? styles.favorited : false)} icon={livestream.isFavorite ? solidStar : emptyStar}/>
+					) : null
+				}
+			</div>
+			{
+				!isHovered ? (
+					<div className={styles.streamerName}
+						 title={(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}>{(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}</div>
+				) : null
+			}
 			<div className={styles.rightPart}>
 				<div className={styles.streamTitle} title={livestream.title}>{livestream.title}</div>
 				<div className={styles.streamGame} title={livestream.game_name}>{livestream.game_name}</div>
