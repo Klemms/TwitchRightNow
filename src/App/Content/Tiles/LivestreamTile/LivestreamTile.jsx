666fc342982c94962ea1bf49b3165e20efb63b4d
Clément Ababou <clement.ababou@outlook.com>
2024-03-28 01:14:13 +0100

    Converted LivestreamTile to a function component

--- src/App/Content/Tiles/LivestreamTile/LivestreamTile.jsx
+++ src/App/Content/Tiles/LivestreamTile/LivestreamTile.jsx
@@ -1,41 +1,29 @@
 import React from "react";
 import styles from './LivestreamTile.module.sass';
 import Button from "../../../Button/Button";
+import {getStreamUptime} from "../../../Util";
 
-export default class LivestreamTile extends React.Component {
-	getStreamUptime(startTime) {
-		let start = new Date(startTime);
-		let now = new Date();
-		let hours = (((now - start) / 1000) / 60) / 60;
-		let minutes = (((now - start) / 1000) / 60) % 60;
-
-		return parseInt(hours) + ":" + parseInt(minutes).toString().padStart(2, '0');
-	}
-
-	render() {
-		let ls = this.props.livestream;
-
-		return (
-			<Button className={styles.stream} onClick={() => {
-				chrome.tabs.create({
-					url: `https://www.twitch.tv/${ls.user_login}`
-				})
-			}}>
-				<img className={styles.streamPic} src={ls.thumbnail_url.replace('{width}', '128').replace('{height}', '72')}/>
-				<div className={styles.streamerName} title={(ls.user_name === '' ? ls.user_login : ls.user_name)}>{(ls.user_name === '' ? ls.user_login : ls.user_name)}</div>
-				<div className={styles.rightPart}>
-					<div className={styles.streamTitle} title={ls.title}>{ls.title}</div>
-					<div className={styles.streamGame} title={ls.game_name}>{ls.game_name}</div>
-					<div className={styles.viewerCount}><span className={styles.liveCircle}></span>{new Intl.NumberFormat('fr-FR').format(ls.viewer_count)}</div>
-					<div className={styles.streamTags}>
-						<div className={styles.streamTime}>{this.getStreamUptime(ls.started_at)}</div>
-						<div className={styles.streamLanguage}>{ls.language}</div>
-						{
-							ls.is_mature ? <div className={styles.streamMature}>18+</div> : null
-						}
-					</div>
+export default React.memo(function LivestreamTile({livestream}) {
+	return (
+		<Button className={styles.stream} onClick={() => {
+			chrome.tabs.create({
+				url: `https://www.twitch.tv/${livestream.user_login}`
+			})
+		}}>
+			<img className={styles.streamPic} src={livestream.thumbnail_url.replace('{width}', '128').replace('{height}', '72')}/>
+			<div className={styles.streamerName} title={(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}>{(livestream.user_name === '' ? livestream.user_login : livestream.user_name)}</div>
+			<div className={styles.rightPart}>
+				<div className={styles.streamTitle} title={livestream.title}>{livestream.title}</div>
+				<div className={styles.streamGame} title={livestream.game_name}>{livestream.game_name}</div>
+				<div className={styles.viewerCount}><span className={styles.liveCircle}></span>{new Intl.NumberFormat('fr-FR').format(livestream.viewer_count)}</div>
+				<div className={styles.streamTags}>
+					<div className={styles.streamTime}>{getStreamUptime(livestream.started_at)}</div>
+					<div className={styles.streamLanguage}>{livestream.language}</div>
+					{
+						livestream.is_mature ? <div className={styles.streamMature}>18+</div> : null
+					}
 				</div>
-			</Button>
-		);
-	}
-}
+			</div>
+		</Button>
+	);
+});
